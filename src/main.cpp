5b0992a261b59267087c7f1ae721d6c08b413a6e
Andrew King <pepsiotaku@gmail.com>
2022-10-23 21:12:39 -0400

    Add glfw example & macos config

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 int main(const int argc, const char* argv[])
 {
@@ -10,5 +11,40 @@ int main(const int argc, const char* argv[])
 		std::cout << "  " << argv[i] << '\n';
 	}
 
-	return 0;
+	GLFWwindow* window = nullptr;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+	/* Set the background color (Cornflower Blue - 100,149,237) */
+	glClearColor(100.0f / 255.0f, 149.0f / 255.0f, 237.0f / 255.0f, 1.0f);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+
 }
